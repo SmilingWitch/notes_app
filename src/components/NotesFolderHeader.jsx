95616baf5e4133d552e0f1745c2723b42e44c8c0
SmilingWitch <danielitayero@gmail.com>
2024-06-21 20:52:46 -0300

    feat: add NotesFolderHeader

---
+++ src/components/NotesFolderHeader.jsx
@@ -0,0 +1,30 @@
+import { View, StyleSheet } from "react-native"
+import StyledText from "./StyledText"
+import Ionaicons from '@expo/vector-icons/Ionicons'
+import theme from "./theme"
+
+const NotesFolderHeader = () => {
+    return (
+        <View style = {styles.header}>
+            <StyledText fontSize='h1' fontWeight='bold'>My Notes</StyledText>
+            <Ionaicons name = "menu" style = {styles.icon}></Ionaicons>
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+      header: {
+        paddingVertical: 20,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+      },
+      icon: {
+        color: theme.colors.textPrimary,
+        fontSize: theme.fontSize.h2
+      }
+})
+
+
+export default NotesFolderHeader
