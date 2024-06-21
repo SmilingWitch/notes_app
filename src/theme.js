6366d6f3502e4eefdf7374661d4c68d51441b478
SmilingWitch <danielitayero@gmail.com>
2024-06-21 20:55:03 -0300

    feat: add theme.js

---
+++ src/theme.js
@@ -0,0 +1,29 @@
+
+const theme = {
+    colors: {
+        textPrimary: '#D2D2D2',
+        textSecundary: '#586069',
+        primary: '#1D1E20',
+        secundary: '#2E71A7',
+        white: "#fefefe",
+        light: '80A0B5',
+        grey: '#333332'
+    },
+    fontSize: {
+        h1: 32,
+        h2: 24,
+        h3: 18.72,
+        regular: 16,
+        small: 10
+    },
+    fonts: 'System',
+    fontWeight: {
+        normal: '400',
+        bold: '700'
+    },
+    
+
+}
+
+
+export default theme
