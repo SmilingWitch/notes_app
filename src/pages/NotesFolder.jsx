f93cdc5e0f8cad67a29936bb8dd5bc89a1e8c02a
SmilingWitch <danielitayero@gmail.com>
2024-06-21 20:56:26 -0300

    feat: add NotesFolder.jsx

---
+++ src/pages/NotesFolder.jsx
@@ -0,0 +1,17 @@
+import NotesFolderHeader from "../components/NotesFolderHeader"
+import NotesFolderList from "../components/NotesFolderList"
+
+
+
+const NotesFolder = () => {
+    return(
+        <>
+            <NotesFolderHeader/>
+            <NotesFolderList/>
+        </>
+        
+    )
+}
+
+
+export default NotesFolder
