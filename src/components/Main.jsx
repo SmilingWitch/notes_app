5a45ac3dc7bddeb249a8aadd0b6992773010ae3e
SmilingWitch <danielitayero@gmail.com>
2024-06-22 20:43:40 -0300

    feat: add /note route

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,31 +1,30 @@
 import { View, Text, StyleSheet,Dimensions } from "react-native"
 import Constants from 'expo-constants'
-import theme from "../theme"
-
 import {Routes,Route, Navigate} from "react-router-native"
 import NotesFolder from "../pages/NotesFolder"
 import Notes from "../pages/Notes"
+import Note from "../pages/Note"
+import Appbar from "./Appbar"
 
 const Main = () => {
     return(
         <View style = {styles.container}>
-          
+
             <Routes>
               <Route path = '/' element = {<NotesFolder/>}/>
-              <Route path = '/note' element = {<Notes/>}/>
+              <Route path = '/notes' element = {<Notes/>}/>
+              <Route path = '/note' element = {<Note/>}></Route>
             </Routes>
-          
-            
+            <Appbar/>
         </View>
     )
 }
-
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: Constants.statusBarHeight,
-
+        
       },
 
 })
