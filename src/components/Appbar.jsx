f55611f0403d69df1c77fbf97bca45d947e98b29
SmilingWitch <danielitayero@gmail.com>
2024-06-22 20:39:57 -0300

    feat: add Appbar.jsx

---
+++ src/components/Appbar.jsx
@@ -0,0 +1,48 @@
+import { View, StyleSheet } from "react-native"
+import StyledText from "./StyledText"
+import Icon from '@expo/vector-icons/AntDesign'
+import theme from "../theme"
+import { Link } from "react-router-native"
+
+const Appbar = () => {
+    return(
+        <View style = {styles.container}>
+            <Link to = '/'>
+                <Icon name = "home" style = {styles.icon}></Icon>
+            </Link>
+            
+            <View style = {styles.icon_pluss}>
+                <Icon name = "plus" style = {styles.icon}></Icon>
+            </View>
+            
+            <Icon name = "search1" style = {styles.icon}></Icon>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: theme.colors.grey,
+        height: 60,
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'space-between'
+    },
+    icon: {
+        fontSize: theme.fontSize.h2,
+        color: theme.colors.textPrimary
+    },
+    icon_pluss:{
+        fontSize: theme.fontSize.h2,
+        backgroundColor: theme.colors.secundary,
+        width: 50,
+        height: 50,
+        bottom: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 1000
+        
+    }
+})
+
+export default Appbar
