81849f378aa82bbf911fb8b1907b97431f0c1d6d
SmilingWitch <danielitayero@gmail.com>
2024-06-22 20:54:08 -0300

    ref: change colors in theme

--- src/theme.js
+++ src/theme.js
@@ -1,27 +1,27 @@
 
 const theme = {
     colors: {
-        textPrimary: '#D2D2D2',
+        textPrimary: '#fefefe',
         textSecundary: '#586069',
         primary: '#1D1E20',
-        secundary: '#2E71A7',
+        secundary: '#209ff0',
         white: "#fefefe",
         light: '80A0B5',
-        grey: '#333332'
+        grey: '#272727'
     },
     fontSize: {
         h1: 32,
         h2: 24,
         h3: 18.72,
         regular: 16,
-        small: 10
+        small: 12
     },
     fonts: 'System',
     fontWeight: {
         normal: '400',
         bold: '700'
     },
-    
+    padding: 10
 
 }
 
