655808adddfb2876ceec61e6cefcbffad9a8c04c
SmilingWitch <danielitayero@gmail.com>
2024-06-24 19:45:18 -0300

    feat: add navigation prop to all components i nNotes

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,17 +1,30 @@
-import { Text } from "react-native"
-
+import { Text, View, StyleSheet } from "react-native"
+import Appbar from '../components/Appbar'
 
 import NotesList from "../components/NotesList"
 import NotesItemHeader from "../components/NotesItemHeader"
+import theme from "../theme"
+import DrowerNavigator from "../drower/DrowerNavigator"
 
-const Notes = () => {
+const Notes = ({route,navigation}) => {
+
+
     return(
-        <>
-            <NotesItemHeader></NotesItemHeader>
-            <NotesList/>
-        </>
+        <View style={styles.container}>
+            <NotesItemHeader navigation = {navigation} route = {route}></NotesItemHeader>
+            <NotesList navigation = {navigation} route = {route}/>
+            <Appbar navigation = {navigation}></Appbar>
+        </View>
         
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+     flex: 1,
+     backgroundColor: theme.colors.primary
+    }
+   });
 
 export default Notes
