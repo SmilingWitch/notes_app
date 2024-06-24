7c88bdb3910ec43797883bdd61df4dd6dd0e9df5
SmilingWitch <danielitayero@gmail.com>
2024-06-24 19:36:52 -0300

    feat: add navigation options to NotesFolderHeader

--- src/components/NotesFolderHeader.jsx
+++ src/components/NotesFolderHeader.jsx
@@ -1,20 +1,30 @@
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet, TouchableOpacity } from "react-native"
 import StyledText from "./StyledText"
-import Icon from '@expo/vector-icons/AntDesign'
+import Icon from '@expo/vector-icons/Feather'
 import theme from "../theme"
 
-const NotesFolderHeader = () => {
+const NotesFolderHeader = ({navigation}) => {
+
     return (
-        <View style = {styles.header}>
-            <StyledText fontSize='h2' fontWeight='bold'>Folders</StyledText>
-            <Icon name="ellipsis1" style = {styles.icon}></Icon>
+        <View style = {styles.container}>
+          <View style = {styles.header}>
+            <TouchableOpacity onPress = {() => navigation.goBack()}>
+              <Icon name = 'arrow-left' style = {styles.icon}></Icon>
+            </TouchableOpacity>
+           
+            <StyledText fontSize='h2' fontWeight='bold' style = {styles.text}>Folders</StyledText>
+          </View>
+            
+            <TouchableOpacity onPress = {() => navigation.navigate('Note')}>
+              <Icon name="plus" style = {styles.icon}></Icon>
+            </TouchableOpacity>
         </View>
 
     )
 }
 
 const styles = StyleSheet.create({
-      header: {
+      container: {
         paddingVertical: 20,
         alignItems: 'center',
         justifyContent: 'space-between',
@@ -22,10 +32,17 @@ const styles = StyleSheet.create({
         paddingHorizontal: theme.padding,
         backgroundColor: theme.colors.primary
       },
+      header: {
+        flexDirection: 'row',
+        alignItems: 'center'
+      },
       icon: {
         color: theme.colors.textPrimary,
         fontSize: theme.fontSize.h2
-      }
+      },
+      text: {
+        marginLeft: 15
+    }
 })
 
 
