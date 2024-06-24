4d85dfd24054aaee41a6d8f22c88b6c54b736e2e
SmilingWitch <danielitayero@gmail.com>
2024-06-23 22:52:08 -0300

    ref: remove import from Note.jsx

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,10 +1,6 @@
 import { View, StyleSheet } from "react-native"
-import StyledText from "../components/StyledText"
 import theme from "../theme"
 import NoteHeader from "../components/NoteHeader"
-import Icon from '@expo/vector-icons/AntDesign'
-
-import { Link } from "react-router-native"
 import NoteContent from "../components/NoteContent"
 
 
