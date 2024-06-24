8a1f77c0bb11dc822984bc15a083780df3f03b13
SmilingWitch <danielitayero@gmail.com>
2024-06-24 19:41:46 -0300

    feat: add navigation options to NotesList

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,40 +1,93 @@
-import { FlatList, StyleSheet, useWindowDimensions } from "react-native"
+import { Dimensions, FlatList, StyleSheet, View, useWindowDimensions } from "react-native"
 import NotesItem from "./NotesItem"
 import { useEffect, useState } from "react";
 import theme from "../theme";
+import StyledText from "./StyledText";
+import { useMemo } from 'react';
 
 const data = [
     {
         id: 1,
-        name: 'All',
-        amount: 100
+        category: 'Books',
+        name: 'Lorem 1',
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis id consequatur corporis labore quaerat quam itaque molestias incidunt recusandae inventore ab vitae architecto minima cum officia sed, accusantium omnis!"
     },
     {
         id: 2,
-        name: 'Novel',
-        amount: 20
+        category: 'Exercise',
+        name: 'Lorem 2',
+        content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. "
     },
     {
         id: 3,
-        name: 'Lists',
-        amount: 5
+        category: 'Lists',
+        name: 'Lorem 3',
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis id consequatur corporis labore quaerat quam itaque molestias incidunt recusandae inventore ab vitae architecto minima cum officia sed, accusantium omnis!"
     },
     {
         id: 4,
-        name: 'Exercise',
-        amount: 10
+        category: 'Novel',
+        name: 'Lorem 4',
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis id consequatur corporis labore quaerat quam itaque molestias incidunt recusandae inventore ab vitae architecto minima cum officia sed, accusantium omnis!"
     },
     {
         id: 5,
-        name: 'Books',
-        amount: 3
-    }
+        category: 'Books',
+        name: 'Lorem 5',
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis id consequatur corporis labore quaerat quam itaque molestias incidunt recusandae inventore ab vitae architecto minima cum officia sed, accusantium omnis!"
+    },
+    {
+        id: 6,
+        category: 'Books',
+        name: 'Lorem 6',
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis id consequatur corporis labore quaerat quam itaque molestias incidunt recusandae inventore ab vitae architecto minima cum officia sed, accusantium omnis!"
+    },
+    {
+        id: 7,
+        category: 'Exercise',
+        name: 'Lorem 7',
+        content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. "
+    },
+    {
+        id: 8,
+        category: 'Lists',
+        name: 'Lorem 8',
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis id consequatur corporis labore quaerat quam itaque molestias incidunt recusandae inventore ab vitae architecto minima cum officia sed, accusantium omnis!"
+    },
+    {
+        id: 9,
+        category: 'Novel',
+        name: 'Lorem 9',
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis id consequatur corporis labore quaerat quam itaque molestias incidunt recusandae inventore ab vitae architecto minima cum officia sed, accusantium omnis!"
+    },
+    {
+        id: 10,
+        category: 'Books',
+        name: 'Lorem 10',
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis id consequatur corporis labore quaerat quam itaque molestias incidunt recusandae inventore ab vitae architecto minima cum officia sed, accusantium omnis!"
+    },
+    {
+        id: 11,
+        category: 'Novel',
+        name: 'Lorem 11',
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis id consequatur corporis labore quaerat quam itaque molestias incidunt recusandae inventore ab vitae architecto minima cum officia sed, accusantium omnis!"
+    },
 ]
 
-const NotesList = () => {
-
+const NotesList = ({navigation, route}) => {
+    const { name } = route.params;
     const { width } = useWindowDimensions();
     const [numColumns, setNumColumns] = useState(2);
+
+
+    // Memoriza los datos filtrados usando useMemo
+    const filteredData = useMemo(() => {
+        if (name === 'All') {
+            return data;
+        }
+        return data.filter(item => item.category === name);
+    }, [name, data]);
+
 
     useEffect(() => {
       // Calcula el número de columnas basado en el ancho de la pantalla
@@ -43,28 +96,37 @@ const NotesList = () => {
     }, [width]);
 
     const renderItem = ({item}) => (
-        <NotesItem id = {item.id} to = '/note'></NotesItem>
+        <NotesItem id={item.id} navigation={navigation} name={item.name} content={item.content} />   
     )
 
     return(
         <FlatList
-            data = {data}
+            data = {filteredData}
             renderItem={renderItem}
             keyExtractor={(item) => item.id}
             key={`myFlatList-${numColumns}`} // Actualiza la clave aquí
             numColumns={numColumns}
-            columnWrapperStyle={styles.container}
+            columnWrapperStyle={styles.containerWrapp}
+            contentContainerStyle = {styles.container}
+            ListEmptyComponent={
+                <StyledText>
+                    List is empty
+                </StyledText>
+            }
         ></FlatList>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    containerWrapp:{
        justifyContent: 'space-between',
        paddingHorizontal: theme.padding,
-       backgroundColor: theme.colors.primary,
-    }
+
+    },
+    container: {
+        backgroundColor: theme.colors.primary,
+    },
+
 })
 
 export default NotesList
