b55123d75e9ba789860785782b9dff0b72b2be66
SmilingWitch <danielitayero@gmail.com>
2024-06-24 19:46:04 -0300

    feat: add route prop to all components in NotesFolder

--- src/pages/NotesFolder.jsx
+++ src/pages/NotesFolder.jsx
@@ -2,20 +2,24 @@
 import { Button } from "react-native"
 import NotesFolderHeader from "../components/NotesFolderHeader"
 import NotesFolderList from "../components/NotesFolderList"
+import { View, StyleSheet } from "react-native"
+import theme from "../theme"
 
 
-const NotesFolder = ({navigation}) => {
+const NotesFolder = ({navigation, route}) => {
     return(
-        <>
-           
-            <NotesFolderHeader/>
-            <NotesFolderList navigation = {navigation}/>
-        </>
+        <View style = {styles}>
+            <NotesFolderHeader navigation = {navigation}  route = {route}/>
+            <NotesFolderList navigation = {navigation}  route = {route}/>
+        </View>
         
     )
 }
 
 
-
+const styles = StyleSheet.create({
+    flex: 1,
+    backgroundColor: theme.colors.primary
+})
 
 export default NotesFolder
