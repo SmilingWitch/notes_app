a413d677b23e9f2fd0e29aab469dbe019a15f4a7
SmilingWitch <danielitayero@gmail.com>
2024-06-23 22:44:50 -0300

    feat: add text to NoteContent.jsx

--- src/components/NoteContent.jsx
+++ src/components/NoteContent.jsx
@@ -1,11 +1,25 @@
+import { View, StyleSheet } from "react-native"
 import StyledText from "./StyledText"
+import theme from "../theme"
 
 
 const NoteContent = () => {
     return(
-        <StyledText>Conetnt</StyledText>
+        <View style = {styles.container}>
+            <StyledText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error odit dolorum praesentium ipsa nam vitae necessitatibus earum nobis, quibusdam saepe est pariatur eos inventore. Non eos autem magnam? Optio, consequuntur.</StyledText>
+
+        </View>
+        
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 20,
+        backgroundColor: theme.colors.primary
+    },
+
+})
 
 export default NoteContent
