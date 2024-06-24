6de13a72551b648276a493ea3e74f29ab648e508
SmilingWitch <danielitayero@gmail.com>
2024-06-23 22:44:09 -0300

    ref: remove Routes from Main and add Stack

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,37 @@
 import { View, Text, StyleSheet,Dimensions } from "react-native"
 import Constants from 'expo-constants'
-import {Routes,Route, Navigate} from "react-router-native"
 import NotesFolder from "../pages/NotesFolder"
 import Notes from "../pages/Notes"
 import Note from "../pages/Note"
 import Appbar from "./Appbar"
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const Main = () => {
     return(
-        <View style = {styles.container}>
-
-            <Routes>
-              <Route path = '/' element = {<NotesFolder/>}/>
-              <Route path = '/notes' element = {<Notes/>}/>
-              <Route path = '/note' element = {<Note/>}></Route>
-            </Routes>
-            <Appbar/>
+        <View style={styles.container}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen 
+                        name="NotesFolder"
+                        component={NotesFolder} 
+                        options={{ headerShown: false }} // Mostrar el AppBar
+                    />
+                    <Stack.Screen 
+                        name="Notes" 
+                        component={Notes} 
+                        options={{ headerShown: false }} // Mostrar el AppBar
+                    />
+                    <Stack.Screen 
+                        name="Note" 
+                        component={Note} 
+                        options={{ headerShown: false }} // Mostrar el AppBar
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+            <Appbar />
         </View>
     )
 }
@@ -24,9 +40,11 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: Constants.statusBarHeight,
-        
       },
 
 })
 
 export default Main
+
+
+        
