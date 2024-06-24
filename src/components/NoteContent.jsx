1cbbafd59e88f4efe720e450bae9a689ec4c94d3
SmilingWitch <danielitayero@gmail.com>
2024-06-24 19:34:40 -0300

    feat: obtain content from para,s in NoteContent

--- src/components/NoteContent.jsx
+++ src/components/NoteContent.jsx
@@ -3,11 +3,13 @@ import StyledText from "./StyledText"
 import theme from "../theme"
 
 
-const NoteContent = () => {
+const NoteContent = ({route}) => {
+
+    const {content} = route.params
+
     return(
         <View style = {styles.container}>
-            <StyledText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error odit dolorum praesentium ipsa nam vitae necessitatibus earum nobis, quibusdam saepe est pariatur eos inventore. Non eos autem magnam? Optio, consequuntur.</StyledText>
-
+            <StyledText>{content}</StyledText>
         </View>
         
     )
@@ -17,7 +19,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: 20,
-        backgroundColor: theme.colors.primary
+        backgroundColor: theme.colors.primary,
+        paddingHorizontal: theme.padding,
     },
 
 })
