170336902a090b95d79f00cc7fa3058b3aeb3123
SmilingWitch <danielitayero@gmail.com>
2024-06-24 19:40:39 -0300

    feat: add navigation options toNotesItemHeader

--- src/components/NotesItemHeader.jsx
+++ src/components/NotesItemHeader.jsx
@@ -1,21 +1,31 @@
+
 import { View, StyleSheet, TouchableOpacity } from "react-native"
 import StyledText from "./StyledText"
 import Icon from '@expo/vector-icons/AntDesign'
 import theme from "../theme"
 import ScrollCategories from "./ScrollCategories"
 
-const NotesItemHeader = () => {
+
+const NotesItemHeader = ({route,navigation}) => {
+
     return(
         <View style = {styles.container}>
-            <View style = {styles.name}>
-                <TouchableOpacity to = '/'>
-                    <Icon name = "arrowleft" style = {styles.icon}></Icon>
-                </TouchableOpacity>
-                <StyledText fontSize='h2' fontWeight='bold' style = {styles.text}>Folder Name</StyledText> 
+
+            <View style = {styles.header}>
+                <View style = {styles.name}>
+                    <StyledText fontSize='h2' fontWeight='bold' style = {styles.text}>Fast Notes</StyledText> 
+                </View>
+                {/*<TouchableOpacity>
+                    <Icon name = "ellipsis1" style = {styles.icon}></Icon>
+                </TouchableOpacity>*/}
             </View>
+            
             <View>
-                <ScrollCategories/>
+                <ScrollCategories navigation = {navigation} route = {route}/>
             </View>
+
+            
+           
         </View>
     )
 }
@@ -24,6 +34,13 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: theme.colors.primary,
     },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        paddingHorizontal: theme.padding
+    },
     icon: {
         color: theme.colors.textPrimary,
         fontSize: theme.fontSize.h2
@@ -31,15 +48,10 @@ const styles = StyleSheet.create({
     name: {
         flexDirection: 'row',
         alignItems: 'center',
-        paddingVertical: 20,
         alignItems: 'center',
-        flexDirection: 'row',
-        paddingHorizontal: theme.padding
-    
+        flexDirection: 'row'
     },
-    text: {
-        marginLeft: 15
-    }
+    
 
 })
 
