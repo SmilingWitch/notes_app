c26110b7c6bad291878919befbd7227a596a35ac
SmilingWitch <danielitayero@gmail.com>
2024-07-02 16:21:28 -0300

    feat: add NotesFolderItemWithoutTouch.jsx

---
+++ src/components/NotesFolderItemWithoutTouch.jsx
@@ -0,0 +1,53 @@
+
+import { View, StyleSheet, TouchableOpacity} from "react-native"
+import StyledText from "./StyledText"
+import Ionaicons from '@expo/vector-icons/Ionicons'
+import theme from "../theme"
+
+
+
+const NotesFolderItemWithoutTouch = ({name, amount}) => {
+
+    return(
+        <TouchableOpacity style = {styles.container}>
+          <>
+          <View style = {styles.header} >
+            <Ionaicons name="folder-open-outline" style = {styles.icon}></Ionaicons>
+            <StyledText >{name}</StyledText>    
+          </View>
+            <View>
+                <StyledText style = {styles.text} fontSize = 'small' fontWeight='bold'>{amount}</StyledText>
+            </View>
+          </>
+            
+        </TouchableOpacity>
+    )
+}
+const styles = StyleSheet.create({
+        container: {
+            marginBottom: 10,
+            padding: 10,
+            borderRadius: 20,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+        },
+      header: {
+        alignItems: 'center',
+        flexDirection: 'row'
+      },
+      icon: {
+        color: theme.colors.textPrimary,
+        fontSize: theme.fontSize.h3,
+        marginRight:10
+      },
+      text: {
+        color: theme.colors.lightGrey
+    
+      }
+})
+
+
+
+
+export default NotesFolderItemWithoutTouch
