2287e637d5a4b9af7c2f279bcbc6fd330c48b323
SmilingWitch <danielitayero@gmail.com>
2024-07-02 16:23:01 -0300

    feat: add ManageFolders route to Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import 'react-native-gesture-handler';
+
 import { View, StyleSheet} from "react-native"
 import Constants from 'expo-constants'
 import NotesFolder from "../pages/NotesFolder"
@@ -6,6 +6,7 @@ import Notes from "../pages/Notes"
 import Note from "../pages/Note"
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ManageFolders from "../pages/ManageFolders"
 
 const Stack = createNativeStackNavigator();
 
@@ -23,6 +24,11 @@ const Main = () => {
                     <Stack.Screen 
                         name="NotesFolder"
                         component={NotesFolder} 
+                        options={{ headerShown: false }} // Mostrar el AppBar
+                    />
+                    <Stack.Screen 
+                        name="ManageFolders"
+                        component={ManageFolders} 
                         options={{ headerShown: false }} // Mostrar el AppBar
                     />
                     
