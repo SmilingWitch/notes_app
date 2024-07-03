836db98e0c27ba332907ed367285df89aff547f2
SmilingWitch <danielitayero@gmail.com>
2024-07-03 18:53:43 -0300

    ref: delete FormatingBar from Note.jsx

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -2,13 +2,16 @@ import { View, StyleSheet } from "react-native"
 import theme from "../theme"
 import NoteHeader from "../components/NoteHeader"
 import NoteContent from "../components/NoteContent"
+import FormatingBar from "../components/FormatingBar"
+import { useRef } from "react"
 
 
 const Note = ({navigation, route}) => {
     return(
         <View style = {styles.container}>
-            <NoteHeader navigation = {navigation} route = {route}/>
-            <NoteContent route = {route}/>
+            <NoteHeader navigation = {navigation} route = {route} />
+            <NoteContent route = {route} />
+            {/*<FormatingBar onFormat={handleFormat}/>*/}
         </View>
         
     )
